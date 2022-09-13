889be4ab663333b7a97c50535d18adf73e624fe5
EgboguFavour <egbogujr@gmail.com>
2022-09-13 16:02:28 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - alphabet
- * print_alphabet_x10 - print a - z ten times 
+ * print_alphabet_x10 - function to print abc 10 times
+ *
+ * Return: 0 
  */
 
 void print_alphabet_x10(void)
